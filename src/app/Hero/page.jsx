b6db91f06325a926f940d5a4eaad8b5de6909ce9
kalpesh.skillsconnect <kalpesh.skillsconnect@gmail.com>
2024-09-28 00:21:41 +0530

    font changes

--- src/app/Hero/page.jsx
+++ src/app/Hero/page.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+"use client"
+
+import React, {useEffect} from 'react'
+import gsap from "gsap";
 
 function page() {
+
+  // useEffect(()=>{
+  //   const t1 = gsap.timeline();
+  //   t1.from(".singleLine .ani-text ",{
+  //     y:10,
+  //     ease:"power4.out",
+  //     delay:1.8,
+  //     stagger:{
+  //       amount:0.4,
+  //     }
+  //   })
+  // })
   return (
     <div className='pt-4 h-dvh'>
         <div className='px-[20px]'>
-            <h1 className='text-center text-[60px] lg:text-[72px] text-[#212529] leading-[75px] lg:pt-[130px] pb-2'>Your imagination, <span className='text-[#ED5729]'>Amplified</span>. Digitally.</h1>
-            <p className='text-[#ED5729] text-[26px] py-5 text-center'>We solve real-world problems efficiently</p>
+            <div className="singleLine"><p className='font-sofia font-medium ani-text text-center text-[60px] lg:text-[72px] text-[#212529] leading-[75px] lg:pt-[130px] pb-2'>Your imagination, <span className='text-[#ED5729]'>Amplified</span>. Digitally.</p></div>
+            <div className="singleLine"><p className='font-sofia ani-text text-[#ED5729] text-[26px] py-5 text-center'>We solve real-world problems efficiently</p></div>
             <div className='text-center '>
                 <button className='rounded-lg bg-[#ED5729] text-white px-8 py-1 mt-2 '>Get Amplified!!!</button>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sofia: ['"Sofia Pro"', 'sans-serif'], // Add Sofia Pro globally
+      },
+      fontWeight: {
+        ultralight: 200,
+        light: 300,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        black: 900,
+      },
     },
   },
   plugins: [],
