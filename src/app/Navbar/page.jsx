69c994f1364303f536056ba0208e7471620c450e
kalpesh.skillsconnect <kalpesh.skillsconnect@gmail.com>
2024-09-30 07:41:18 +0530

    logo

--- src/app/Navbar/page.jsx
+++ src/app/Navbar/page.jsx
@@ -11,11 +11,11 @@ function page() {
     <div >
         <nav className=' flex w-full items-center justify-between px-[20px] py-[20px] lg:mx-auto lg:px-[40px]'>
            <div className="w-full grid lg:grid-cols-3 grid-cols-2 items-center">
-            <div className='h-[27px] w-[115px]'>Logo</div>
+            <div className=''><img src="/images/logo/smoking-chimney-logo.png" alt="Traced" /></div>
             <nav className='hidden lg:block'>
                 <ul className='flex gap-[24px] justify-center items-center'>
                     {navLinks.map((item,index)=>(
-                        <li className='flex justify-center items-center ' key={index}>
+                        <li className='flex justify-center items-center px-4 ' key={index}>
                             <a className='group text-[15px] h-[20px] inline-block overflow-y-hidden relative'>
                             <div className='group-hover:translate-y-[-100%] transition-all ease-[cubic-bezier(0.16, 1, 0.3, 1)] duration-500 opacity-70'>
                                 {item.name}
