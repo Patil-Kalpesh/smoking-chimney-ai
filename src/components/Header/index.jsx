2ef0e3f3df24a6c3a5d4cec3b385f266c902709d
Patil-Kalpesh <117829968+Patil-Kalpesh@users.noreply.github.com>
2024-12-22 13:36:33 +0530

    logo erro

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -23,7 +23,7 @@ function ResponsiveNavbar() {
         {/* Logo */}
         <div className="w-full grid lg:grid-cols-3 grid-cols-2 items-center">
         <div className="">
-        <Link href="/"><Image src="/images/logo/logo.svg" width={40} height={40} alt="Smoking Chimney Logo" /></Link>
+        <Link href="/"><Image src="/images/logo/Logo.svg" width={40} height={40} alt="Smoking Chimney Logo" /></Link>
         </div>
 
         {/* Mobile Menu Button */}
